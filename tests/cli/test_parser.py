5684a751133b615cc0526a0c0b57628056987834
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2023-12-20 14:56:30 +0100

    test(test_parser.py): modify tests to accomodate the two parsers

--- tests/cli/test_parser.py
+++ tests/cli/test_parser.py
@@ -3,9 +3,14 @@
 
 """Tests for dicom2elk.cli.parser module."""
 
-from dicom2elk.cli.parser import get_parser
+from dicom2elk.cli.parser import get_dicom2elk_parser, get_file2list_parser
 
 
-def test_get_parser():
-    # Test if get_parser returns a parser
-    assert get_parser() is not None
+def test_get_dicom2elk_parser():
+    # Test if get_dicom2elk_parser returns a parser
+    assert get_dicom2elk_parser() is not None
+
+
+def test_get_file2list_parser():
+    # Test if get_file2list_parser returns a parser
+    assert get_file2list_parser() is not None
