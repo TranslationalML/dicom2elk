6f748af153865a6ab90d04ae9a6883f6ca77a1dc
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2023-12-20 14:58:52 +0100

    feat: add test_file2list.py with skeleton for testing of file2list script

---
+++ tests/cli/test_file2list.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2023, The TranslationalML team and Contributors. All rights reserved.
+#  This software is distributed under the open-source Apache 2.0 license.
+
+"""Tests for file2list CLI."""
+
+import os
+import pytest
+
+
+@pytest.mark.script_launch_mode("subprocess")
+def test_file2list(script_runner, tmpdir, test_dcm_files, io_path):
+    pass
