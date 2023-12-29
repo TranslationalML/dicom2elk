7876c68fff5e7528e073aab1dc5c63274ee51983
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2023-12-29 13:17:46 +0100

    refactor: move database related functions from file2list to dedicated utils.database module

--- dicom2elk/cli/file2list.py
+++ dicom2elk/cli/file2list.py
@@ -3,62 +3,20 @@ import sqlite3 as sq
 import sys
 from datetime import datetime
 import time
+import warnings
+import tqdm
 
+from dicom2elk.info import __version__
 from dicom2elk.cli.parser import get_file2list_parser
-
-
-DB_FILE = 'file_abs_database.db'
-BATCH_SIZE = 500
-
-
-def get_db_size(db_connection):
-    x = db_connection.execute("SELECT count(*) FROM pacs_file_paths;")
-    for row in x:
-        print('Nbr file in db : ' + str(row[0]))
-
-
-def get_db_size_to_clean(db_connection):
-    x = db_connection.execute("SELECT count(*) FROM pacs_file_paths where batch is null or batch = 'TMP_';")
-    for row in x:
-        return int(row[0])
-
-
-def stage_line(db_connection, batch):
-    cmd = (
-            "UPDATE pacs_file_paths SET batch = 'TMP_' WHERE path IN (SELECT path FROM pacs_file_paths WHERE batch is null ORDER BY path LIMIT " + str(
-        batch) + ")")
-    db_connection.execute(cmd)
-
-
-def clean_db(db_connection, batch, out='.'):
-    dump_staged_file(db_connection, out)
-    while get_db_size_to_clean(db_connection) > 0:
-        stage_line(db_connection, batch)
-        dump_staged_file(db_connection, out)
-
-
-def dump_staged_file(db_connection, out='.'):
-    now = datetime.now()  # current date and time
-    current_time = now.strftime("%Y%m%d_%H%M%S%f")
-    x = db_connection.execute("SELECT path FROM pacs_file_paths where batch = 'TMP_';")
-    data_file = ''
-    for row in x:
-        row = str(row[0])
-        if data_file == '':
-            data_file = open(out + '\\dicom_' + current_time + '.txt', 'w')
-        data_file.write("%s\n" % row)
-    if data_file != '':
-        data_file.close()
-        db_connection.execute(
-            "UPDATE pacs_file_paths SET batch = '" + 'dicom_' + current_time + '.txt' + "' WHERE batch = 'TMP_';")
-
-
-def closing_connection(db_connection, batch, out):
-    clean_db(db_connection, batch, out)
-    get_db_size(db_connection)
-    db_connection.commit()
-    db_connection.close()
-    return 0
+from dicom2elk.utils.database import (
+    create_table,
+    stage_line,
+    dump_staged_file,
+    clean_db,
+    closing_connection,
+)
+from dicom2elk.utils.misc import prepare_file_list_batches
+from dicom2elk.utils.logging import create_logger
 
 
 def main():
